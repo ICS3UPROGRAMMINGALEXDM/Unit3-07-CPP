b1b12b6dea77eb925f234dccd04767bf356b8bdc
Ubuntu <ubuntu@ip-172-31-17-170.ec2.internal>
2022-04-06 21:01:17 +0000

    finished

--- dating.cpp
+++ dating.cpp
@@ -1,5 +1,11 @@
+// Copyright (c) 2022 Alex De Meo All rights reserved
+//
+// Created By: Alex De Meo
+// Date: 02//22
+// Description: program determines whether or not the user can date the
+// grandchild
 #include <iostream>
-#include "colors.h"
+#include "Unit3-07-CPP/colors.h"
 using std::string;
 using std::cout;
 using std::cin;
@@ -7,27 +13,33 @@ using std::endl;
 
 
 
-int main()
-{
+int main() {
     // declaring vars
     string gLooks, rich;
     bool loop = true;
-    
+
+    // loop to ensure they enter a valid answer
     while (loop) {
+        // Get user input
         cout << "Are you rich? (y/n): ";
         cin >> rich;
-        
+        // Get user input
         cout << "Are you good looking? (y/n): ";
         cin >> gLooks;
-        
+        // make sure input is valid
         if (gLooks == "y" || gLooks == "n" && rich == "y" || rich == "n") {
-            cout << "Hello";
-            loop = false;
+            // if the conditions are met then say congrats, else say no
+            if (gLooks == "y" || rich == "y") {
+                cout << BOLDGREEN <<
+                "Congratulations, You can date our grandchild";
+                loop = false;
+            } else {
+                cout << BOLDYELLOW <<
+                "Imagine not being rich or good looking lol. Sorry bud.";
+                loop = false;
+            }
         } else {
             cout << BOLDRED << "Invalid Input. Try again." << RESET << endl;
         }
-        
     }
-    
-    
 }
